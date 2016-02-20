5fe786b1e680a58264d4edf1dd0444539667d001
Simon Kurka <dev@simon.kurka.cc>
2016-02-20 12:56:21 +0100

    Adds first ip of the subnet to loopback interface

--- manifests/dhcp.pp
+++ manifests/dhcp.pp
@@ -8,6 +8,11 @@ define dhcp::dhcp (
   require dhcp
   require dhcp::params
 
+  require network
+
+  $self = ip_network($subnet, 1)
+  $self6 = ip_network($subnet6, 1)
+
   dnsmasq::dhcp { "dhcp-${title}": 
     paramtag   => $interface,
     paramset   => 'dhcp',
@@ -17,12 +22,20 @@ define dhcp::dhcp (
     lease_time => $leasetime,
   }
 
+  network::inet::loopback::post_up { "dhcp_${title}":
+    cmd => "/bin/ip addr add ${self} dev \$IFACE",
+  }
+
   $prefix = ip_network($subnet6, 0)
 
   concat::fragment { "dhcp-${title}":
     target  => '/etc/radvd.conf',
     content => epp('dhcp/radvd.epp'),
     order   => '20',
+  }
+
+  network::inet6::loopback::post_up { "dhcp_${title}":
+    cmd => "/bin/ip -6 addr add ${self6} dev \$IFACE",
   }
 
 }
