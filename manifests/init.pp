22f05ec366cb8e8fd7154095b9437f8ffe2b619f
Simon Kurka <dev@simon.kurka.cc>
2016-02-24 20:49:39 +0100

    duplicate declaration error resolved

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,11 @@ class dhcp (
   
 ) inherits dhcp::params {
 
-  require dhcp::install
-  require dhcp::config
+  class { 'dhcp::install': } ->
+  class { 'dhcp::config': }
+
+  contain dhcp::install
+  contain dhcp::config
 
   create_resources('dhcp::dhcp', hiera('dhcp::dhcp', {}))
 
