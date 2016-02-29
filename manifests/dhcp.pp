51f46a15da2888cf02587f7d39e4aa49948a4fc1
Simon Kurka <dev@simon.kurka.cc>
2016-02-29 18:30:10 +0100

    push only ip address to dnsmasq

--- manifests/dhcp.pp
+++ manifests/dhcp.pp
@@ -13,15 +13,18 @@ define dhcp::dhcp (
   dnsmasq::dhcp { "dhcp-${title}":
     paramtag   => $interface,
     paramset   => 'dhcp',
-    dhcp_start => ip_network($subnet, 2),
-    dhcp_end   => ip_broadcast($subnet, 1),
+    dhcp_start => ip_address(ip_network($subnet, 2)),
+    dhcp_end   => ip_address(ip_broadcast($subnet, 1)),
     netmask    => ip_netmask($subnet),
     lease_time => $leasetime,
   }
 
   concat::fragment { "dhcp-${title}":
     target  => '/etc/radvd.conf',
-    content => epp('dhcp/radvd.epp', { interface => $interface, subnet6 => $subnet6 }),
+    content => epp('dhcp/radvd.epp', {
+      interface => $interface,
+      subnet6 => $subnet6
+    }),
     order   => '20',
   }
 
